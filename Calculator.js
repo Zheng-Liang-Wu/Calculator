f6d626f5d97ecad54b7dff7467ccd9f12af694a0
Lee <a22852206@gmail.com>
2024-02-03 01:23:27 +0800

    Change IU/IO, New some function

--- Calculator.js
+++ Calculator.js
@@ -1,18 +1,37 @@
 function toggleContent() {
-    // 根據選擇的自摸有無切換顯示狀態
-    var selfDrawnWinValue = document.querySelector('input[name="selfDrawnWin"]:checked').value;
+  var selfDrawnWinValue = document.querySelector('input[name="selfDrawnWin"]:checked').value;
+  var whoBanker = document.getElementById('whoBanker');
+  var bankerArea = document.getElementById('bankerArea');
+  var paymentDropdown = document.getElementById('paymentDropdown');
     
-    var whoBanker = document.getElementById('whoBanker');
-    var bankerArea = document.getElementById('bankerArea');
-    var paymentDropdown = document.getElementById('paymentDropdown');
-    
-    if (selfDrawnWinValue === 'yes') {
+  if (selfDrawnWinValue === 'yes') {
       whoBanker.classList.remove('hidden');
       bankerArea.classList.remove('hidden');
       paymentDropdown.classList.add('hidden');
-    } else {
+  } else {
       whoBanker.classList.add('hidden');
       bankerArea.classList.add('hidden');
       paymentDropdown.classList.remove('hidden');
     }
-  } 
+} 
+
+function calculateMoney() {
+  var WonUnit = parseInt(document.getElementById('wonUnit').value);
+  var continueBanker = parseInt(document.getElementById('continueBanker').value);
+  var selfDrawnWin = document.querySelector('input[name="selfDrawnWin"]:checked').value;
+    
+  var basicUnits = 30;
+  var eachUnit = 10;
+  var normalWin = basicUnits + eachUnit * WonUnit;
+
+  var amounts = calculatePlayerAmounts(wonUnit, normalWin, continueBanker, selfDrawnWin);
+  updateAmounts(amounts.playerAmounts);
+  }
+
+function calculateExtraUnits(continueBanker) {
+  var minExtraUnitsPerContinue = 1;
+  var ExtraUnitsPerContinue = 2;
+  var extraUnits = minExtraUnitsPerContinue + ExtraUnitsPerContinue * continueBanker;
+  return extraUnits;
+  }
+
