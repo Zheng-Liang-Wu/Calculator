043db17d52af77aefcd4e8f2b305c08a439d3eed
Lee <a22852206@gmail.com>
2024-02-03 00:31:36 +0800

    Basic Calculator ver1.0

---
+++ Calculator.js
@@ -0,0 +1,18 @@
+function toggleContent() {
+    // 根據選擇的自摸有無切換顯示狀態
+    var selfDrawnWinValue = document.querySelector('input[name="selfDrawnWin"]:checked').value;
+    
+    var whoBanker = document.getElementById('whoBanker');
+    var bankerArea = document.getElementById('bankerArea');
+    var paymentDropdown = document.getElementById('paymentDropdown');
+    
+    if (selfDrawnWinValue === 'yes') {
+      whoBanker.classList.remove('hidden');
+      bankerArea.classList.remove('hidden');
+      paymentDropdown.classList.add('hidden');
+    } else {
+      whoBanker.classList.add('hidden');
+      bankerArea.classList.add('hidden');
+      paymentDropdown.classList.remove('hidden');
+    }
+  }
