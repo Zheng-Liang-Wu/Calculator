a5c2a7d8cfa7c44915f91a7a9bbe844dd8032788
Lee <a22852206@gmail.com>
2024-02-08 17:52:39 +0800

    0208 金額更新

--- Calculator.js
+++ Calculator.js
@@ -3,24 +3,25 @@ var eachUnit = 10;
 var normalWin;
 var selectedBanker; // 保存選中的莊家
 var extraUnits;
+var playerAmounts = {};
 
 function toggleContent() {
   var selfDrawnWinValue = document.querySelector('input[name="selfDrawnWin"]:checked').value;
   var whoBanker = document.getElementById('whoBanker');
   var bankerArea = document.getElementById('bankerArea');
   var paymentDropdown = document.getElementById('paymentDropdown');
-    
-  if (selfDrawnWinValue === 'yes') {
-      whoBanker.classList.remove('hidden');
-      bankerArea.classList.remove('hidden');
-      paymentDropdown.classList.add('hidden');
-  } else {
-      whoBanker.classList.add('hidden');
-      bankerArea.classList.add('hidden');
-      paymentDropdown.classList.remove('hidden');
-    }
-} 
 
+  if (selfDrawnWinValue === 'yes') {
+    whoBanker.classList.remove('hidden');
+    bankerArea.classList.remove('hidden');
+    paymentDropdown.classList.add('hidden');
+} else {
+    whoBanker.classList.add('hidden');
+    bankerArea.classList.add('hidden');
+    paymentDropdown.classList.remove('hidden');
+  }
+  
+}
 function updateBanker() {
   var bankerSelect = document.getElementById('banker');
   selectedBanker = bankerSelect.value;
